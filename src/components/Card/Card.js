5317d0e7a96f52d2ad70adcf9ac89ad30ced460f
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-02-22 10:44:13 -0700

    Render data on cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
 export const Card = ({ name, founded, seats, titles, coatOfArms, ancestralWeapons, words }) => {
+  const cleanFounded = () => {
+    return founded !== '' ? founded : 'N/A'
+  }
+
+  const renderTitles = () => {
+    return titles.map( title => <p>Titles: { title }</p> )
+  }
+
+  const renderWeapons = () => {
+    return ancestralWeapons.map( weapon => <p>Ancestral Weapons: { weapon }</p> )
+  }
+
+  const renderWords = () => {
+    return words !== '' ? <p>Words: { words }</p> : null
+  }
+
   return (
     <div>
-      <p>Card</p>
+      <h2>{ name }</h2>
+      <h3>Founded: { cleanFounded() }</h3>
+      <p>Seats: { seats }</p>
+      { renderTitles() }
+      <p>Coat of Arms: { coatOfArms }</p>
+      { renderWeapons() }
+      { renderWords() }
     </div>
   )
 }
