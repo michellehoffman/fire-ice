261e499d4e7b68a60fddcf2279546613fdbc72e9
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-02-22 14:40:13 -0700

    Finish mapDispatchToProps test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -7,12 +7,7 @@ describe('App', () => {
   let wrapper;
 
   beforeEach(() => {
-    const mockHouses = [
-      { name: "House Corbray of Heart's Home" },
-      { name: "House Dayne of Starfall" }
-    ];
-
-    wrapper = shallow(<App addHouses={ jest.fn() } houses={ mockHouses }/>, { disableLifecycleMethods: true })
+    wrapper = shallow(<App />, { disableLifecycleMethods: true })
   })
 
   it('should match snapshot when houses are NOT loaded', () => {
@@ -22,6 +17,12 @@ describe('App', () => {
   })
 
   it('should match snapshot when houses are loaded', () => {
+    const mockHouses = [
+      { name: "House Corbray of Heart's Home" },
+      { name: "House Dayne of Starfall" }
+    ];
+    
+    wrapper = shallow(<App addHouses={ jest.fn() } houses={ mockHouses } />, { disableLifecycleMethods: true })
     wrapper.setState({ loaded: true })
 
     expect(wrapper).toMatchSnapshot();
@@ -43,7 +44,7 @@ describe('App', () => {
   it('should call dispatch when an action is called', () => {
     const mockDispatch = jest.fn();
 
-    console.log(wrapper.props);
+    mapDispatchToProps(mockDispatch).addHouses();
 
     expect(mockDispatch).toHaveBeenCalled();
 
